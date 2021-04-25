23986900cae00911e364107b29e433304c76a201
Martin <martinale4@hotmail.com>
2021-04-25 16:57:25 -0500

    Database Back-up

--- src/keys.js
+++ src/keys.js
@@ -3,9 +3,9 @@ module.exports = {
     database: {
 
         host: 'localhost',
-        user: 'amazonadmin',
-        password: 'Chechonito14',
-        database: 'amazonprime'
+        user: 'work4me_Admin',
+        password: 'Work4me@',
+        database: 'work4me'
 
     }
 
