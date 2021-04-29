cc69d4f90e96d4a7ced761ebd9fd26ec292fe662
Juan Pablo Rivera <juanpari1766@gmail.com>
2021-04-29 15:28:33 -0500

    index

--- w4mFront/src/index.js
+++ w4mFront/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from './views/Login';
+import Login from './views/Login'
+import Index from './views/Index'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- w4mFront/src/views/Index.jsx
+++ w4mFront/src/views/Index.jsx
@@ -1,16 +1,26 @@
 import {React, Component} from 'react';
 import logo from '../assets/w4mLogo.png';
-import name from '../assets/w4mLogoWords.png';
-import "../css/login.scss";
-import Input from '../components/Input';
+import "../css/index.scss";
 import Button from '../components/Button';
-import RaisedButton from '../components/RaisedButton';
-import imageBack from '../assets/BackDesignAritos'
+import imageBack from '../assets/BackDesignAritos.png'
+import worker from '../assets/index.jpg'
 
 export default class Index extends Component{
     render(){
         return(
-            <div></div>
+            <div className="grid">
+                <div className="nav-bar">
+                    <img src={logo} alt="w4m"/>
+                    <Button className="signIn" text="Sign in"/>
+                    <Button text="Sign up"/>
+                </div>
+                <div className="imageBack">
+                    <img id="back" src={imageBack} alt="style"/>
+                </div>
+                <div className="imageBack">
+                    <img id="worker" src={worker} alt=""/>
+                </div>
+            </div>
         );
     }
 }
