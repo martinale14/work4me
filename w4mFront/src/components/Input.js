23da27369a37d354214d3768ce01a486aa0bea5c
Juan Pablo Rivera <juanpari1766@gmail.com>
2021-05-01 02:39:23 -0500

    arreglos finales login

--- w4mFront/src/components/Input.js
+++ w4mFront/src/components/Input.js
@@ -4,26 +4,14 @@ import '../css/inputs.scss';
 
 
 export default class Input extends Component {
-    
+
 
     render() {
         return (
-            <TextField id="inputs" type={this.props.type} label={this.props.placeholder} className={this.props.className}/>
-
-            /* <div
-                className={this.props.className}>
-                <input
-                    id={this.props.id}
-                    className="mdl-textfield__input inputs"
-                    type={this.props.type}
-                    autoComplete="off"
-                />
-                <label
-                    className="mdl-textfield__label labels">
-                    {this.props.placeholder}
-                </label>
-            </div> */
-
+            <TextField id="inputs" 
+            type={this.props.type} 
+            label={this.props.placeholder} 
+            className={this.props.className} />
         );
     }
 }
--- w4mFront/src/views/Login.jsx
+++ w4mFront/src/views/Login.jsx
@@ -1,6 +1,7 @@
 import { React, Component } from 'react';
 import logo from '../assets/w4mLogo.png';
 import name from '../assets/w4mLogoWords.png';
+import back from '../assets/BackDesignLogin.png'
 import "../css/login.scss";
 import Input from '../components/Input';
 import Btn from '../components/Btn';
@@ -9,36 +10,40 @@ import RaisedButton from '../components/RaisedButton';
 export default class Login extends Component {
   render() {
     return (
-      <div className="maincito">
-        <div className="container">
-          <img
-            className="logo"
-            src={logo}
-            alt="Work4Me" />
-          <img
-            className="logoWord"
-            src={name}
-            alt="Work4Me" />
+      <div className="body">
+        <div id="imgCont">
+          <img src={back} alt=""/>
         </div>
-        <br />
-        <br />
-        <h2 className='title'>
-          Sign in
+        <div className="maincito">
+          <div className="container">
+            <img
+              className="logo"
+              src={logo}
+              alt="Work4Me" />
+            <img
+              className="logoWord"
+              src={name}
+              alt="Work4Me" />
+          </div>
+          <br />
+          <br />
+          <h2 className='title'>
+            Sign in
         </h2>
-        <br />
-        <Input id="Email" type="text" placeholder="Email address" className="inp"/>
-        <Input id="Password" type="password" placeholder="Password" className="inp"/>
-        <br />
-        <br />
-        <Btn text="Sign in" onClick={() => {
-          console.log('Button was clicked.');
-        }} />
-        <br />
-        <div className="link">
-          Don't you have an account? &nbsp;
-          <RaisedButton text="Click here" onClick={() => {
-            console.log('The link was clicked.');
+          <Input id="Email" type="text" placeholder="Email address" className="inp" />
+          <Input id="Password" type="password" placeholder="Password" className="inp" />
+          <br />
+          <br />
+          <Btn text="Sign in" onClick={() => {
+            console.log('Button was clicked.');
           }} />
+          <br />
+          <div className="link">
+            Don't you have an account? &nbsp;
+            <RaisedButton text="Click here" onClick={() => {
+              console.log('The link was clicked.');
+            }} />
+          </div>
         </div>
       </div>
     );
